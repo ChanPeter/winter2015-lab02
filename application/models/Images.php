da48655d4106771c34f0f259319f35e61648ddeb
Peter Chan <chan-p@outlook.com>
2015-01-18 12:57:12 -0800

    Comp4711 tutorial2c
    Worked up to page 22 of the tutorial.  Can't get the menunav and the title showing.  Also the gallery and the about links not working.  I believe the welcome page images are hard coded still.  Not sure why that isn't working.
    Signed-off-by:Peter Chan <chan-p@outlook.com>

---
+++ application/models/Images.php
@@ -0,0 +1,18 @@
+<?php
+
+class Images extends CI_Model {
+	
+	// constructor
+	function _construct()
+	{
+		parent::_construct();
+	}
+	
+	// return all images, descending order by post date
+	function all()
+	{
+		$this->db->order_by("id", "desc");
+		$query = $this->db->get('images');
+		return $query->result_array();
+	}
+}
